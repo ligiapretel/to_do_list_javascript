10a74a60f423da3f7818489fcb0b526c815a48be
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2020-01-17 12:10:06 -0300

    Deletando tarefa da lista e removendo do array do localstorage

--- js/script.js
+++ js/script.js
@@ -26,7 +26,8 @@ buttonAdd.onclick = function(){
     listaTarefas.push(valorDigitado);
 
     // Como passei um parâmetro na função gerar tarefa, quando chamo ela aqui preciso passar o parâmetro novamente
-    gerarTarefa(valorDigitado);
+    // Para saber qual a posição do array entra a nova tarefa, dou um .lenght e subtraio 1. Esse é o valor que entrará no atributo posição
+    gerarTarefa(valorDigitado, listaTarefas.length - 1);
 
     // Para não ficar redundante, comento os códigos abaixo relacionados a criação da tarefa e troco pela função gerar tarefa
             // // alert('Estou funcionando');
@@ -65,17 +66,26 @@ buttonAdd.onclick = function(){
 
 // Criando funções que vai imprimir as informações que tenho no meu array listaTarefas
 function imprimirListaTarefas(listaTarefas){
+    // Limpo todo o board para renderizar tudo de novo
+    board.innerHTML = "";
     // Foreach do javascript
-   for(let tarefa of listaTarefas){
-       gerarTarefa(tarefa);
-   } 
+//    for(let tarefa of listaTarefas){
+//        gerarTarefa(tarefa);
+//    } 
+    // Foreach verdadeiro do javascript (pega a posição e o valor)
+    // O js vai percorrer o array e para cada item do array ele vai executar uma função anônima pegando o valor e a posição. Todo forEach pega o valor e a posição nessa ordem.
+    listaTarefas.forEach(function(valor, posicao){
+        gerarTarefa(valor,posicao)
+    })
 }
 
 // Criando função que gera uma tarefa
-function gerarTarefa(valorDigitado){
+function gerarTarefa(valorDigitado, posicao){
     // Criando uma div que armazenará uma nova tarefa ao clicar no botão add
     let tarefa = document.createElement('div');
     tarefa.setAttribute('class','tarefa m-1');
+    // Criando atributo posicao, que será recebido na função - a posição será um número dinâmico
+    tarefa.setAttribute('posicao',posicao);
 
     let titulo = document.createElement('div');
     titulo.setAttribute('class','col-md-8');
@@ -98,6 +108,16 @@ function gerarTarefa(valorDigitado){
     // Para saber qual botão foi clicado, adicionamos um evento com event.target. Coloco essa função no gerar tarefa para que eu já saiba qual botão exatamente foi clicado (se eu criasse essa função fora, teria que identificar antes qual era o botão clicado, e aqui eu já não preciso)
     // event.target - retorna o elemento que foi clicado
     checkButton.onclick = function(event){
+        console.log(listaTarefas);
+        let posicaoTarefa = tarefa.getAttribute('posicao');
+        // A função filter retorna um Array, então eu faço listaTarefas = listaTarefas para atualizar esse Array.
+        listaTarefas = listaTarefas.filter(function(valor,posicao){
+            return posicao != posicaoTarefa;
+        })
+
+        imprimirListaTarefas(listaTarefas);
+        // Atualizando o array com o item removido (localStorage), senão toda vez que der F5 esse item vai voltar
+        localStorage.setItem('listaTarefas',JSON.stringify(listaTarefas));
         // Como já selecionei a div tarefa lá em createImageBitmap, posso removê-la direto aqui
         tarefa.remove();
         // Uma forma de selecionar a div tarefa é com event.target.parentNode
@@ -113,9 +133,3 @@ function gerarTarefa(valorDigitado){
     // Colocando o elemento tarefa dentro do elemento board
     board.appendChild(tarefa);
 }
-
-// // Criando função para apagar uma tarefa 
-// function deletarTarefa(tarefa){
-//     let tarefa = document.getElementsByClassName('tarefa');
-//     tarefa.removeChild(list.childNodes[0]); 
-// }
